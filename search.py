2ebb18e8491cbd6cd814a4b0e95ccf1df864cc53
unknown <lajja.tharwala@ontariotechu.net>
2023-11-21 20:48:26 -0500

    Lab 09 completed code

--- search.py
+++ search.py
@@ -1,2 +1,33 @@
 # INSERT YOUR IMPLEMENTATION OF binary_search HERE
+def binary_search(values, to_find, start_index, end_index, num_comparisons):
 
+    if start_index > end_index:
+      num_comparisons+=1
+      return False, num_comparisons
+    
+    middle = (start_index + end_index) // 2
+
+    if to_find == values[middle]:
+      num_comparisons+=1
+      return True, num_comparisons
+    
+    elif to_find < values[middle]:
+      num_comparisons+=1
+      return binary_search(values, to_find, start_index, middle - 1, num_comparisons)
+    
+    else:
+      num_comparisons+=1
+      return binary_search(values, to_find, middle + 1, end_index, num_comparisons)
+    
+
+num_comparisons = 0
+values = [2,4,6,8,10,12,14,16,18,20]
+print(binary_search(values, 14, 0, len(values) - 1, num_comparisons))
+print(binary_search(values, 7, 0, len(values) - 1, num_comparisons))
+
+
+max_elements = 10000000
+for length in range(1, max_elements, 500000):
+    values = list(range(1, (2 * length) + 1, 2))
+    num_comparisons, found = binary_search(values, length + 1, 0, len(values) - 1, num_comparisons)
+    print(f'{length:08d}', '*' * (num_comparisons // 10))
--- sort.py
+++ sort.py
@@ -1,2 +1,26 @@
 # ADD YOUR IMPLEMENTATION OF insertion_sort HERE
+def insertion_sort(values, num_comparisons):
+    for j in range(1, len(values)):
+        key = values[j]
+        i = j - 1
+        
+        while i >= 0 and values[i] > key:
+            values[i+1] = values[i]
+            i = i-1
+            values[i+1] = key
+            num_comparisons+=2
 
+    return values, num_comparisons
+
+num_comparisons = 0
+unsorted = [4,3,7,1,8,2]
+sorted = insertion_sort(unsorted, num_comparisons)
+print(sorted)
+
+max_elements = 30
+
+for length in range(1, max_elements):
+    unsorted = list(range(length, 0, -1))
+    sorted, num_comparisons = insertion_sort(unsorted, num_comparisons)
+
+    print(f'{num_comparisons:03d}', '*' * (num_comparisons // 10))
